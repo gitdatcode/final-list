4804c201423031e0721533542bd92d98660df9b8
kinsomicrote <kinsomicrote.inc@gmail.com>
2018-09-11 14:45:31 +0100

    created Movies component

---
+++ src/components/movies/Movies.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Movies List</h2>
+    <template v-if="loadingMovies">
+
+    </template>
+    <template v-else>
+      
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Movies',
+    data() {
+      return {
+        movies: [],
+        loadingMovies: true,
+        loadedMovies: false
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
