afe9e982e8b8cfc4cfe9d7eaac43218459083322
Jer'Maine Montiel <jermaine.m.jones@outlook.com>
2018-09-14 17:38:18 -0500

    created nav and footer for app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <template>
   <div id="app" class="container">
+  <div class="nav">
+    <nav>
+      <router-link to="/">Home</router-link>
+    </nav>
+  </div>
+  <div class="content">
     <router-view/>
+  </div>
+  <div class="footer">
+    <div class="signature">a project by <a href='https://www.datcode.io'>DATCODE, INC.</a></div>
+  </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      routes: this.$router.routes
+    }
+  }
 }
 </script>
